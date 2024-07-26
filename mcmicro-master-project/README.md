ca14794be5dfcb3b5a6a972e2c7baaf11ec9d118
Nathan T Johnson <johnsonnathant@gmail.com>
2024-07-26 00:25:51 -0400

    Add files via upload

---
+++ mcmicro-master-project/README.md
@@ -0,0 +1,10 @@
+# CyCIF Manager
+
+Purpose: Provide pipeline platform infrastructure to streamline CyCIF Analysis
+both on a local machine and on the O2 cluster at HMS
+
+# Pipeline Workflow
+![CyCIF Pipeline Plan](/images/CyCif_Pipeline_Plan.png)
+
+For more details, please refer to the [mcmicro
+manual](https://labsyspharm.github.io/mcmicro/).
---
+++ mcmicro-master-project/bin/clean.sh
@@ -0,0 +1,18 @@
+#script used for testing
+#purpose is to remove all files that are not the raw images and markes.csv
+echo 'Remove Job files'
+rm -f *.o
+rm -f *.e
+rm -f *.sh
+rm -f *.txt
+echo 'Remove folder structure except raw images'
+rm -fr */cell_states  
+rm -fr */clustering  
+rm -fr */dearray  
+rm -fr */feature_extraction  
+#rm -fr */illumination_profiles  
+rm -fr */prob_maps  
+rm -fr */registration
+rm -fr */segmentation
+rm -fr output
+echo 'Cleaning Completed'
